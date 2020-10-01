849a63ee91fe82469c2f3e106db9c0d122afd618
Lino Rallo <lino@gmail.com>
2020-10-01 15:16:47 -0300

    Fixes
    added counter, removed unnecesary prints

--- crawler.py
+++ crawler.py
@@ -4,17 +4,17 @@ from html_analizer import extract
 import persistence
 from phonenumberextractor import PhoneNumberExtractor
 
-
+i=0
 for url in persistence.readURL():
+    i+=1
+    print(i)
     response=requests.get(url)
     soup=BeautifulSoup(response.text,'html.parser')
     soup=str(soup)
     results = [extract(soup)]
-    print(results)
     extractor = PhoneNumberExtractor()
     matches = extractor.extract_phone_numbers(soup)
     phones = ', '.join(matches)
-    print(phones)
     results.append(phones)
     print(results)
     persistence.writeResults(url,results)
